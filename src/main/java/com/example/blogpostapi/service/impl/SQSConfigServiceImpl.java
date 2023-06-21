3827876ec882ababbd778bfe7d4d1b97d9b7365e
alexmontes1215 <alex.montes@ext.blaze.me>
2023-06-20 20:21:48 -0500

    update sqs service

--- src/main/java/com/example/blogpostapi/service/impl/SQSConfigServiceImpl.java
+++ src/main/java/com/example/blogpostapi/service/impl/SQSConfigServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.blogpostapi.service.impl;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.AmazonSQSException;
@@ -19,21 +20,21 @@ public class SQSConfigServiceImpl implements SQSConfigService {
     @Autowired
     PropertiesConfiguration configuration;
 
-    private static final String QUEUE_NAME = "my_queue@sqs";
+    private static final String QUEUE_NAME = "my_first_queue";
 
     @Override
     public String createQueue() {
         AWSCredentials credentials         = new BasicAWSCredentials(configuration.getApiKey(), configuration.getSecretKey());
         AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
 
-        AmazonSQS sqs = AmazonSQSClientBuilder.standard().withCredentials(credentialsProvider).build();
-        CreateQueueRequest create_request = new CreateQueueRequest(QUEUE_NAME)
+        AmazonSQS sqs = AmazonSQSClientBuilder.standard().withCredentials(credentialsProvider).withRegion(Regions.SA_EAST_1).build();
+        CreateQueueRequest createQueueRequest = new CreateQueueRequest(QUEUE_NAME)
                 .addAttributesEntry("DelaySeconds", "60")
                 .addAttributesEntry("MessageRetentionPeriod", "86400");
 
         CreateQueueResult result = null;
         try {
-            result = sqs.createQueue(create_request);
+            result = sqs.createQueue(createQueueRequest);
         } catch (AmazonSQSException e) {
             if (!e.getErrorCode().equals("QueueAlreadyExists")) {
                 throw e;
